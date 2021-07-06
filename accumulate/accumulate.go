5949c59d3f26b3715f0844aa1ddcd4118124a131
Daniel Carvalho <danielpacarvalho@gmail.com>
2021-07-06 14:56:57 -0300

    Exercism

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -1,5 +1,21 @@
 package accumulate
 
-func Accumulate(lista []string) []string {
+import "strings"
 
+func Accumulate(lista []string, comando string) []string {
+	retorno := make([]string, len(lista))
+	if strings.Compare(comando, "echo") == 0 {
+		retorno = append(retorno, lista...)
+	}
+	if strings.Compare(comando, "capitalize") == 0 {
+		for _, palavra := range lista {
+			retorno = append(retorno, strings.ToUpper(palavra))
+		}
+	}
+	if strings.Compare(comando, "strings.ToUpper") == 0 {
+		for _, palavra := range lista {
+			retorno = append(retorno, strings.ToUpper(palavra))
+		}
+	}
+	return retorno
 }
